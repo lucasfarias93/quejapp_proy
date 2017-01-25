1432b16e6db046d62eb0854e546561be5aba89a0
lucasfarias93 <extintion007@gmail.com>
2017-01-24 22:01:33 -0300

    Nuevos colores tentativos. Primeras configuraciones del tab. Imagen en Tabs
    Cosas varias de estilos mas algunos botones posibles

--- quejappProyect/app/src/main/java/com/projectbelatrix/quejapp/Tab1.java
+++ quejappProyect/app/src/main/java/com/projectbelatrix/quejapp/Tab1.java
@@ -1,15 +1,24 @@
 package com.projectbelatrix.quejapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.Normalizer;
 
 /**
  * Created by LUCAS on 23/01/2017.
  */
 public class Tab1 extends Fragment {
+    private Button formularioReclamo;
+    private Button hacerOtraCosa;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +26,16 @@ public class Tab1 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.layout_tab1, container, false);
+        View view = inflater.inflate(R.layout.layout_tab1, container, false);
+        formularioReclamo = (Button) view.findViewById(R.id.cargarFormulario);
+        hacerOtraCosa = (Button) view.findViewById(R.id.hacerOtraCosa);
+
+        formularioReclamo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent i = new Intent(TabsActivity.class, FormularioActivity.class);
+            }
+        });
+        return view;
     }
 }
--- quejappProyect/app/src/main/java/com/projectbelatrix/quejapp/TabsActivity.java
+++ quejappProyect/app/src/main/java/com/projectbelatrix/quejapp/TabsActivity.java
@@ -1,12 +1,15 @@
 package com.projectbelatrix.quejapp;
 
 import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TabsActivity extends AppCompatActivity {
 
     private FragmentTabHost tabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,8 +17,12 @@ public class TabsActivity extends AppCompatActivity {
         tabHost= (FragmentTabHost) findViewById(android.R.id.tabhost);
         tabHost.setup(this,getSupportFragmentManager(),android.R.id.tabcontent);
 
+
         tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("¿Que desea hacer?"),Tab1.class, null);
         tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Historial de reclamos"),Tab2.class, null);
         tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Más sobre Quejapp"),Tab3.class, null);
+
     }
+
+
 }
