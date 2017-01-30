2e388cc49eb64c58259ce73c4d5c2ca9d446535f
gastongrippi <gaston.grippi@gmail.com>
2017-01-30 16:42:42 -0300

    Agregada logica y UI para el manejo de distintos reclamos

--- quejappProyect/app/src/main/java/com/projectbelatrix/quejapp/Activities/FormularioActivity.java
+++ quejappProyect/app/src/main/java/com/projectbelatrix/quejapp/Activities/FormularioActivity.java
@@ -1,15 +1,78 @@
 package com.projectbelatrix.quejapp.Activities;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.projectbelatrix.quejapp.R;
+import com.projectbelatrix.quejapp.reclamo1;
+import com.projectbelatrix.quejapp.reclamo2;
+import com.projectbelatrix.quejapp.reclamo3;
 
 public class FormularioActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final FragmentManager fragmentManager = getFragmentManager();
         setContentView(R.layout.activity_formulario);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tipo_reclamo, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case(0):
+                        System.out.println("Eligio una opcion" + position);
+                        break;
+                    case(1):
+                        FragmentTransaction transaction = fragmentManager.beginTransaction();
+                        reclamo1 fragment = new reclamo1();
+                        transaction.replace(R.id.activityContainer, fragment);
+                        transaction.commit();
+                        break;
+                    case(2):
+                        transaction = fragmentManager.beginTransaction();
+                        reclamo2 fragment2 = new reclamo2();
+                        transaction.replace(R.id.activityContainer, fragment2);
+                        transaction.commit();
+                        break;
+                    case(3):
+                        transaction = fragmentManager.beginTransaction();
+                        reclamo3 fragment3 = new reclamo3();
+                        transaction.replace(R.id.activityContainer, fragment3);
+                        transaction.commit();
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        }) ;
     }
 }
---
+++ quejappProyect/app/src/main/java/com/projectbelatrix/quejapp/reclamo2.java
@@ -0,0 +1,36 @@
+package com.projectbelatrix.quejapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+
+
+public class reclamo2 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_reclamo2, container, false);
+        //Nuevos parametros para el view del fragmento
+        RelativeLayout.LayoutParams params =
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.MATCH_PARENT);
+
+        //Nueva Regla: EL fragmento estara debajo del boton add_fragment
+        params.addRule(RelativeLayout.BELOW, R.id.button_confirm);
+
+        //Margenes: top:41dp
+        params.setMargins(0,41,0,0);
+
+        //Setear los parametros al view
+        view.setLayoutParams(params);
+
+        return view;
+    }
+}
